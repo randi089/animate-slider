c3abf2858643b6fa4de250bc0f08d6e2bff1cf17
Randi Febriadi <randifebriadi@gmail.com>
2025-02-06 12:38:00 +0700

    6 Feb Zuhur

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const slider = document.querySelector(".slider");
+const list = document.querySelector(".list");
+const thumbnail = document.querySelector(".thumbnail");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+next.addEventListener("click", () => {
+  initSlider("next");
+});
+
+prev.addEventListener("click", () => {
+  initSlider("prev");
+});
+
+const initSlider = (type) => {
+  const sliderItems = list.querySelectorAll(".item");
+  const thumbnailItems = thumbnail.querySelectorAll(".item");
+
+  if (type === "next") {
+    list.appendChild(sliderItems[0]);
+    thumbnail.appendChild(thumbnailItems[0]);
+    slider.classList.remove("start");
+    slider.classList.add("next");
+  } else {
+    const lastItemIndex = sliderItems.length - 1;
+    list.prepend(sliderItems[lastItemIndex]);
+    thumbnail.prepend(thumbnailItems[lastItemIndex]);
+    slider.classList.remove("start");
+    slider.classList.add("prev");
+  }
+
+  setTimeout(() => {
+    slider.classList.remove("next");
+    slider.classList.remove("prev");
+  }, 2000);
+};
